e059dc3ca287dc5976eaa387a1b5a24ab0a0fbbf
federicoromeo98 <federico.romeo.98@gmail.com>
2020-11-09 00:09:30 +0100

    again

--- GamifiedMarketing/GamifiedMarketingWeb/wip/inspection.js
+++ GamifiedMarketing/GamifiedMarketingWeb/wip/inspection.js
@@ -1,16 +1,13 @@
 
 window.onload = function() { // avoid variables ending up in the global scope
 
-
    var radioContainer = document.getElementById('radio-container');
    radioContainer.addEventListener("click", (e) => {
-      var selectedRadio = radioContainer.querySelector("input[name=quest]:checked");
+      var selectedRadio = radioContainer.querySelector("input[name=questionnaire]:checked");
       var table = document.getElementById('questionnaire-table');
 
       var table = document.getElementById('selected-questionnaire');
       table.innerHTML = selectedRadio.value;
    });
-
-
 
 };
